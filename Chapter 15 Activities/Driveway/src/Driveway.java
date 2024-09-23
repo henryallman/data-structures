dbc443f1a676e1acbd416929e239e2b6b6dbf3cd
henryallman <hnallman@stu.naperville203.org>
2024-09-23 12:09:43 -0500

    done withd riveway

--- Chapter 15 Activities/Driveway/src/Driveway.java
+++ Chapter 15 Activities/Driveway/src/Driveway.java
@@ -22,7 +22,8 @@ public class Driveway
     public Driveway()
     {
         // Complete the constructor
-        ...
+        driveway = new Stack<>();
+        street = new Stack<>();
 
 
     }
@@ -35,7 +36,8 @@ public class Driveway
     public void add(int licensePlate)
     {
         // Complete this method
-        ...
+        driveway.push(licensePlate);
+        print();
 
 
     }
@@ -47,8 +49,20 @@ public class Driveway
     */
     public void remove(int licensePlate)
     {
-        // Complete this method
-        ...
+        if (driveway.contains(licensePlate)) {
+          while (driveway.isEmpty() == false && driveway.peek() != licensePlate) {
+              street.push(driveway.pop());
+              print();
+          }
+          if (driveway.isEmpty() == false) {
+              driveway.pop();
+              print();
+          }
+          while (street.isEmpty() == false) {
+              driveway.push(street.pop());
+              print();
+          }
+      }
 
 
     }
@@ -60,11 +74,16 @@ public class Driveway
     {
         System.out.println("In Driveway, starting at first in (one license plate per line):");
         // Print the cars in the driveway here
-        ...
+        for (int car : driveway) {
+          System.out.println(car);
+      }
 
         System.out.println("In Street, starting at first in (one license plate per line):");
         // Print the cars in the street here
-        ...
+        for (int car : street) {
+          System.out.println(car);
+      }
+      System.out.println();
 
     }
 }
